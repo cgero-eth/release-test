ae15ef5c592722db0754955d8a2880347c5965d9
Ruggero Cino <ruggero.cino@gmail.com>
2025-01-31 16:59:16 +0100

    Implement read changelog script

---
+++ .github/workflows/scripts/readChangelog.js
@@ -0,0 +1,24 @@
+var fs = require("fs");
+
+module.exports = async ({ core }) => {
+  const { version, path } = process.env;
+
+  try {
+    core.info(`Read changes in version ${version}.`);
+    const changelog = fs.readFileSync(path, {
+      encoding: "utf8",
+      flag: "r",
+    });
+
+    const versionChanges = changelog
+      .split(/(?=## \d+\.\d+\.\d+)/g)
+      .find((version) => version.startsWith(`## ${version}`))
+      .replace(`## ${version}`, "")
+      .trim();
+
+    core.setOutput("changes", versionChanges);
+    core.info(`Changes retrieved successfully.`);
+  } catch (error) {
+    core.error(`Error retrieving changes for version ${version}.`);
+  }
+};
--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# release-test
-
-## 0.3.1
-
-### Patch Changes
-
-- 0dd5cc0: Fix
